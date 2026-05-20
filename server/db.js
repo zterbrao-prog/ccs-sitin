df28ab6f11d98f93a0139f2b9498ad30751252cc
Zethier Pelisan <zterbrao@gmail.com>
2026-05-20 13:33:50 +0800

    use railway env variables

--- server/db.js
+++ server/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '112002',
-  database: 'ccs_sitin',
+  host: process.env.MYSQLHOST || 'localhost',
+  user: process.env.MYSQLUSER || 'root',
+  password: process.env.MYSQLPASSWORD || '112002',
+  database: process.env.MYSQLDATABASE || 'ccs_sitin',
+  port: process.env.MYSQLPORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
